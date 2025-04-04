7fb4150a67ac48cef29ee73622fa9f5b9bb78fad
anurpeljto <anur.peljto@stu.ibu.edu.ba>
2025-04-04 18:32:33 +0200

    push deps as well

---
+++ LightFin/fiscalization-listener/src/main/java/com/anurpeljto/fiscalizationlistener/FiscalizationListenerApplication.java
@@ -0,0 +1,13 @@
+package com.anurpeljto.fiscalizationlistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiscalizationListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiscalizationListenerApplication.class, args);
+	}
+
+}
---
+++ LightFin/fiscalization-listener/src/test/java/com/anurpeljto/fiscalizationlistener/FiscalizationListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anurpeljto.fiscalizationlistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiscalizationListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
